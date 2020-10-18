4dd9fa9b234588c12213e40b71d70c57a8d72171
grun00 <lucas.c.grunheidt@gmail.com>
2020-10-18 18:02:10 -0300

    added buble_sort with optimization

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
+// O(n^2)
+pub fn buble_sort<T:PartialOrd>(v: &mut [T]){
+    for p in 0..v.len() {
+        let mut sorted = true;
+        for i in 0..v.len() - 1 - p {
+            if v[i] > v[i + 1]{
+                v.swap(i, i + 1);
+                sorted = false;
+            }
+        }
+
+        if sorted {
+            return;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_sorts_array() {
+        let mut v = vec![5, 4, 3, 2, 1];
+        buble_sort(&mut v);
+        assert_eq!(v, [1, 2, 3, 4, 5]);
     }
 }
