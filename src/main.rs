d5a7035ca92368915236bea54a0418a7a1c23055
grun00 <lucas.c.grunheidt@gmail.com>
2020-10-21 21:03:10 -0300

    added small main fn for sorting benchmarking

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use rand::Rng;
+use std::time::Instant;
+mod lib;
+
+fn main(){
+    let mut rng = rand::thread_rng();
+
+    let vec_size = 100_000;
+    let base_vec: Vec<u64> = (0..vec_size).map(|_| rng.gen_range(0, 10000)).collect();
+    let mut bubble_vec = base_vec.clone();
+    let mut merge_vec = base_vec.clone();
+    let mut quick_vec = base_vec.clone();
+    let mut threaded_quick_vec = base_vec.clone();
+
+    println!("=================+ Staring benchmark");
+
+    let start = Instant::now();
+    lib::buble_sort(&mut bubble_vec);
+    let duration = start.elapsed();
+
+    println!("Bubble Sort time for {} elements: {:?}", vec_size, duration);
+
+    println!("---------------------------");
+
+    let start = Instant::now();
+    lib::merge_sort(merge_vec);
+    let duration = start.elapsed();
+
+    println!("Merge Sort time for {} elements: {:?}", vec_size, duration);
+
+    println!("---------------------------");
+
+    let start = Instant::now();
+    lib::quick_sort(&mut quick_vec);
+    let duration = start.elapsed();
+
+    println!("Quick Sort time for {} elements: {:?}", vec_size, duration);
+
+    println!("---------------------------");
+
+    let start = Instant::now();
+    lib::rayon_quick_sort(&mut threaded_quick_vec);
+    let duration = start.elapsed();
+
+    println!("Multi Thread Quick Sort time for {} elements: {:?}", vec_size, duration);
+
+    println!("=================+ Ending benchmark");
+}
