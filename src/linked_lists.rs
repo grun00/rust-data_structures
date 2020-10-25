6a35295eec46ebb3e953a6e9d1931e3e48b1c11e
grun00 <lucas.c.grunheidt@gmail.com>
2020-10-24 23:15:18 -0300

    added simple linked list + tests

--- src/linked_lists.rs
+++ src/linked_lists.rs
@@ -1,47 +1,92 @@
+use std::fmt::Debug;
+
 #[derive(Debug)]
-pub struct LinkedList<T>(Option<(T, Box<LinkedList<T>>)>);
+pub struct Node<T: PartialOrd>(Option<(T, Box<Node<T>>)>);
 
-impl<T> LinkedList<T> {
+impl<T: std::fmt::Debug + PartialOrd> Node<T> {
     pub fn new() -> Self {
-        LinkedList(None)
+        Node(None)
     }
 
-    pub fn push_front(&mut self, data: T) {
+    pub fn new_head(&mut self, data: T) {
         let t = self.0.take();
-        self.0 = Some((data, Box::new(LinkedList(t))));
+        self.0 = Some((data, Box::new(Node(t))));
     }
 
-    pub fn push_back(&mut self, data: T) {
+    pub fn append(&mut self, data: T) {
         match self.0 {
-            Some((_, ref mut child)) => child.push_back(data),
-            None => self.push_front(data),
+            Some((_, ref mut child)) => child.append(data),
+            None => self.new_head(data),
         }
     }
 
-    pub fn value(self) -> T {
-        self.0.unwrap().0
+    pub fn add_in_order(&mut self, data: T) {
+        match self.0 {
+            Some((ref value, ref mut child)) => {
+                if  data <= *value {
+                    self.new_head(data);
+                } else {
+                    child.add_in_order(data);
+                }
+            },
+            None => self.new_head(data),
+        }
     }
 
+    pub fn make_array(self) -> Vec<T> {
+        let mut array: Vec<T> = Vec::new();
+        let mut nav = self;
+
+        loop {
+            match nav.0 {
+                Some((value,child)) => {
+                    array.push(value);
+                    nav = *child;
+                },
+                None => break
+            }
+        }
+
+        array
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn linked_list_push_front() {
-        let mut ll: LinkedList<u32> = LinkedList::new();
+    fn linked_list_append() {
+        let mut node: Node<u32> = Node::new();
 
-        ll.push_front(1);
-        ll.push_front(2);
-        ll.push_front(3);
+        node.append(1);
+        node.append(2);
 
-        println!("valu: {:?}", ll.value());
-
-        panic!("");
-
-
+        assert_eq!(node.0.unwrap().0, 1);
     }
-    fn linked_list_push_back() {}
-    fn linked_list_sort() {}
-    fn linked_list_add_sorted() {}
+    #[test]
+    fn linked_list_add_head() {
+        let mut node: Node<u32> = Node::new();
+
+        node.new_head(1);
+        node.new_head(2);
+
+        assert_eq!(node.0.unwrap().0, 2);
+    }
+    #[test]
+    fn linked_list_add_in_order() {
+        let mut node: Node<u32> = Node::new();
+
+        for i in 0..=5 {
+            node.append(i * 2);
+        }
+
+        for i in 1..=5 {
+            node.add_in_order(i * 2 - 1);
+        }
+
+        let vec = node.make_array();
+
+        assert_eq!(vec, [0,1,2,3,4,5,6,7,8,9,10]);
+    }
 }
