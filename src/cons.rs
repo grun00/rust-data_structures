3b669b8d1e64361e88583e6f5bcc74f1d0cae758
grun00 <lucas.c.grunheidt@gmail.com>
2020-10-26 21:40:32 -0300

    changed enum name to Node from More

--- src/cons.rs
+++ src/cons.rs
@@ -7,7 +7,7 @@ pub struct List {
 #[derive(Debug)]
 enum Link {
     Empty,
-    More(Box<Node>),
+    Node(Box<Node>),
 }
 #[derive(Debug)]
 struct Node {
@@ -27,13 +27,13 @@ impl List {
             next: mem::replace(&mut self.head, Link::Empty),
         });
 
-        self.head = Link::More(new_node);
+        self.head = Link::Node(new_node);
     }
 
     pub fn pop(&mut self) -> Option<u32> {
         match mem::replace(&mut self.head, Link::Empty) {
             Link::Empty => None,
-            Link::More(node) => {
+            Link::Node(node) => {
                 self.head = node.next;
                 Some(node.data)
             }
@@ -46,7 +46,7 @@ impl List {
         for _ in 0..nth {
             match nav {
                 Link::Empty => {}
-                Link::More(node) => {
+                Link::Node(node) => {
                     nav = &node.next;
 
                 }
@@ -55,7 +55,7 @@ impl List {
 
         match nav {
             Link::Empty => None,
-            Link::More(node) => Some(node.data)
+            Link::Node(node) => Some(node.data)
         }
     }
 }
@@ -63,7 +63,7 @@ impl List {
 impl Drop for List {
     fn drop(&mut self) {
         let mut cur_link = mem::replace(&mut self.head, Link::Empty);
-        while let Link::More(mut boxed_node) = cur_link {
+        while let Link::Node(mut boxed_node) = cur_link {
             cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
         }
     }
@@ -80,7 +80,7 @@ mod tests {
         cons.push(10);
         match &cons.head {
             Link::Empty => { panic!(""); },
-            Link::More(node) => { assert_eq!(10, node.data) }
+            Link::Node(node) => { assert_eq!(10, node.data) }
         }
     }
 
